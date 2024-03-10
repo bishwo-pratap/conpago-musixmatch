5b796eba21d3bee0e5b38b573e29852e23f386ce
Bishwo Pratap Bhagat <contact.bishwo@gmail.com>
2024-03-10 14:18:49 +1000

    route registration helper file

---
+++ backend/src/routes/v1/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const authRoute = require('./auth.route');
+const userRoute = require('./user.route');
+const docsRoute = require('./docs.route');
+
+const router = express.Router();
+
+const defaultRoutes = [
+  {
+    path: '/auth',
+    route: authRoute,
+  },
+  {
+    path: '/user',
+    route: userRoute,
+  },
+  {
+    path: '/docs',
+    route: docsRoute,
+  },
+];
+
+defaultRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
+module.exports = router;
