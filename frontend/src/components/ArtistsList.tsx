b4bdd2144130a934a0c1be39a51e765bbce0411c
Bishwo Pratap Bhagat <contact.bishwo@gmail.com>
2024-03-11 12:33:43 +1000

    initial artists list components

---
+++ frontend/src/components/ArtistsList.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import {
+  Button,
+  Flex,
+  Heading,
+  List,
+  ListIcon,
+  ListItem,
+  Stack,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import { FaCheckCircle } from 'react-icons/fa'
+import NextLink from 'next/link'
+import { Link } from '@chakra-ui/react'
+import { ArtistListProps } from '@/types/artist.types'
+
+const ArtistList = ({ name, twitter, country, rating, checked = false, artistId }: ArtistListProps) => {
+  const colorTextLight = checked ? 'white' : 'purple.600'
+  const bgColorLight = checked ? 'purple.400' : 'gray.300'
+
+  const colorTextDark = checked ? 'white' : 'purple.500'
+  const bgColorDark = checked ? 'purple.400' : 'gray.300'
+
+  return (
+    <Stack
+      p={3}
+      py={3}
+      justifyContent={{
+        base: 'flex-start',
+        md: 'space-around',
+      }}
+      direction={{
+        base: 'column',
+        md: 'row',
+      }}
+      alignItems={{ md: 'start' }}>
+        <Flex minW={200}>
+          <Heading size={'md'}>{name}</Heading>
+        </Flex>
+        <Flex minW={200}>
+          <List spacing={3} textAlign="start">
+            <ListItem key={country}>
+              <ListIcon as={FaCheckCircle} color="green.500" />
+              Country: {country}
+            </ListItem>
+            <ListItem key={twitter}>
+              <ListIcon as={FaCheckCircle} color="green.500" />
+              {twitter}
+            </ListItem>
+          </List>
+        </Flex>
+        <Flex minW={100}>
+          <Heading size={'md'}>{rating || 0}/100</Heading>
+        </Flex>
+      <Flex minW={100}>
+        <Link as={NextLink} href={`/albums/${artistId}`}>
+          <Button
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize={'sm'}
+            size="md"
+            fontWeight={600}
+            color={useColorModeValue(colorTextLight, colorTextDark)}
+            bgColor={useColorModeValue(bgColorLight, bgColorDark)}
+          >
+            View Albums
+          </Button>
+        </Link>
+      </Flex>
+    </Stack>
+  )
+}
+
+export default ArtistList;
