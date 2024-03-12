9fe3bb46346154e346bf5627716c10d962fd50bf
Bishwo Pratap Bhagat <contact.bishwo@gmail.com>
2024-03-12 11:58:09 +1000

    api url config file

---
+++ frontend/src/config/index.ts
@@ -0,0 +1,9 @@
+export const routesConfig = {
+  baseUrl: 'http://localhost:3000/v1',
+  signInEndpoint: 'auth/login',
+  signUpEndpoint: 'auth/register',
+  artistsEndpoint: 'user/top-artists',
+  albumsEndpoint: 'user/artist-albums',
+  tracksEndpoint: 'user/album-tracks',
+  lyricsEndpoint: 'user/track-lyrics',
+}
