439b42c9e396cb8a771e70bee46c3f9ef09729c6
Bishwo Pratap Bhagat <contact.bishwo@gmail.com>
2024-03-12 11:25:03 +1000

    added redux provider

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,13 +1,21 @@
 'use client'
 
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import theme from './theme';
+import { Provider } from 'react-redux';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { PersistGate } from 'redux-persist/integration/react';
+import makeStore from '@/store/store';
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const { store, persistor } = makeStore();
   return (
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      {children}
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            {children}
+          </PersistGate>
+        </Provider>
     </ChakraProvider>
   );
 }
